cd494a39d619b4155e40fc476850bd9415c5c90a
hyotaime <hyotaime@hyotaime.com>
2023-10-18 22:21:00 +0900

    fix: Using list sort instead of collection sort, Update gitignore

--- src/main/java/hyotaime/rankingsystem/controller/HomeController.java
+++ src/main/java/hyotaime/rankingsystem/controller/HomeController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -21,7 +20,7 @@ public class HomeController {
     public String home(Model model) {
         // 서비스 계층의 findMembers 메서드를 이용해 전체 랭킹 정보를 가져옴
         List<Member> member = memberService.findMembers();
-        Collections.sort(member, Comparator.comparing(Member::getPoints).reversed());
+        member.sort(Comparator.comparing(Member::getPoints).reversed());
         model.addAttribute("member", member);
         return "home";
     }
